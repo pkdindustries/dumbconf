ac6959650eabacbf1099e4a1e23b5847d1b7f300
a-lex <alexschlessinger@users.noreply.github.com>
2021-07-17 18:57:05 -0700

    add 'optional', update tests and docs (#5)

--- dumbconf.go
+++ dumbconf.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 	"reflect"
+	"strings"
 )
 
 type dumbError struct {
@@ -14,28 +15,53 @@ func (d *dumbError) Error() string {
 	return d.key
 }
 
+// pass in a pointer to struct, have it populated from system environment.
+//		type Config struct {
+//			DB    string // defaults to env:"DB,required"
+//			API   string `env:"APIBASE"`
+//			MAYBE string `env:"MAYBE,optional"`
+//		}
+// 		var c = Config{}
+//		err := dumbconf.LoadConfig(&c)
 func LoadConfig(conf interface{}) error {
 	mirror := reflect.ValueOf(conf)
 	typeof := mirror.Elem().Type()
 	for i := 0; i < typeof.NumField(); i++ {
 		field := typeof.Field(i)
-		key, ok := field.Tag.Lookup("env")
+		key := ""
+		opt := ""
+
+		env, ok := field.Tag.Lookup("env")
 		if !ok {
 			key = field.Name
-		}
-		value := os.Getenv(key)
-		if value == "" {
-			log.Printf("key '%s' not found in environment", key)
-			return &dumbError{key}
 		} else {
-			log.Printf("key '%s' found in environment", key)
+			tags := strings.Split(env, ",")
+			switch len(tags) {
+			case 1:
+				key = tags[0]
+			case 2:
+				key = tags[0]
+				opt = tags[1]
+			default:
+				log.Printf("dumbconf: malformed tag [%s]", env)
+				return &dumbError{}
+			}
 		}
+
+		value, ok := os.LookupEnv(key)
+		if !ok {
+			log.Printf("dumbconf: [%s] not found", key)
+			if opt != "optional" {
+				return &dumbError{key}
+			}
+		}
+
 		stype := reflect.ValueOf(conf).Elem()
 		sfield := stype.Field(i)
 		if sfield.CanSet() {
 			sfield.SetString(value)
 		} else {
-			return &dumbError{key}
+			return &dumbError{}
 		}
 	}
 	return nil
